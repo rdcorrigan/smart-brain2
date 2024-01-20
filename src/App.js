d330c191fb97dce9f7678139cf34058922b08b8b
Ryan Corrigan <ryandcorrigan@gmail.com>
2024-01-20 07:13:23 -0700

    component did mount removed from app.js

--- src/App.js
+++ src/App.js
@@ -43,12 +43,6 @@ class App extends Component {
     }})
   }
 
-  componentDidMount() {
-    fetch('https://smart-brain2-backend.onrender.com/')
-      .then(response => response.json())
-      .then(console.log)
-  }
-
   calculateFaceLocation = (data) => {
     console.log(data);
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
